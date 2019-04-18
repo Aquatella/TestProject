ee33b68f8c02d45c577c51b2017d3fd66fef5cf7
Olga Tikhonova <o.tikhonova@godeltech.com>
2019-04-18 17:16:53 +0300

    Web API app added

---
+++ WebApiApp/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using Hipernate_test.Models;
+using Hipernate_test.Repository.Interfaces;
+using System.Collections.Generic;
+using System.Web.Http;
+using WebApiApp.Models;
+
+namespace WebApiApp.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        private IUserRepository _userRepository;
+        public ValuesController(IUserRepository userRepository)
+        {
+            this._userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<User> GetAllUsers()
+        {
+            return _userRepository.GetAllUsers();
+        }
+
+        public User GetUser(int id)
+        {
+            return _userRepository.GetUser(id);
+        }
+
+        [HttpPost]
+        public void CreateUser([FromBody]User user)
+        {
+            _userRepository.AddUser(user);
+        }
+
+        [HttpPut]
+        public void EditUser(int id, [FromBody]User user)
+        {
+            _userRepository.UpdateUser(user);
+        }
+
+        public void DeleteUser(int id)
+        {
+            _userRepository.DeleteUser(new User { Id = id });
+        }
+    }
+}
+
